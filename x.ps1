363dd910fcd47339c92da68e7670fe39e79b683b
Sebastien <sebastien.re@alumni.univ-avignon.fr>
2024-04-07 22:44:52 +0200

    netoyage

--- x.ps1
+++ x.ps1
@@ -1,3 +1,5 @@
 slice2py Soup.ice --output-dir=backend
 
-slice2js Soup.ice --output-dir=front/src/scripts
+slice2js Soup.ice --output-dir=front/src/gen
+
+slice2java Soup.ice --output-dir=client-javafx/src/main/java
